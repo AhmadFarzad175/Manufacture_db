f9c352c6ed075aba9f39b349e2eb1cb1d6282037
farzadhakimi1402 <farzadhakimi1402@gmail.com>
2024-05-06 15:47:24 +0430

    owner

--- app/Http/Requests/Peoples/OwnerRequest.php
+++ app/Http/Requests/Peoples/OwnerRequest.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Requests\Peoples;
 
+use App\Traits\UpdateRequestRules;
 use Illuminate\Foundation\Http\FormRequest;
 
 class OwnerRequest extends FormRequest
 {
+    use UpdateRequestRules;
     /**
      * Determine if the user is authorized to make this request.
      */
@@ -21,12 +23,16 @@ class OwnerRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
+        $rules = [
             'name'   => 'required|string|max:255',
             'email'  => 'required|email|unique:owners,email|max:192',
             'phone'  => 'required|string|max:15',
             'share'  => 'required|numeric|min:0|max:100',
             'asset'  => 'required|numeric|min:0',
         ];
+
+        $this->isMethod('PUT') ? $this->applyUpdateRules($rules) : null;
+
+        return $rules;
     }
 }
