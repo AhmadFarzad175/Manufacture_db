f6b91741b7dfc237dd852ea8a06d5d21f47ef261
Mohammad Zarif Hossain Yar <156784074+Zarif2024@users.noreply.github.com>
2024-05-07 20:59:01 +0430

    new changes

---
+++ resources/js/store/ExpenseRepository.js
@@ -0,0 +1,118 @@
+import { defineStore } from "pinia";
+import { reactive, ref } from "vue";
+
+// Import Axios
+import { axios, setContentType } from "../axios";
+// import { toast } from "vue3-toastify";
+import { useRouter } from "vue-router";
+export let useExpenseRepository = defineStore("ExpensRepository", {
+    state() {
+        return {
+            isLoading: false,
+            error: null,
+            loading: false,
+            createDailog: false,
+            updateDailog: false,
+            page: 1,
+            itemsPerPage: 7,
+            selectedItems: [],
+            selectAll: false,
+            showSelect: true,
+            router: useRouter(),
+            totalItems: 0,
+            itemKey: "id",
+
+            Search: "",
+        };
+    },
+    actions: {
+        GetProducts(account, name) {
+            const accArr = account.filter((acc) => acc.id == id);
+            this.name = accArr[0].name;
+            console.log(currArr[0].name);
+        },
+        GetCurrency(currency, id) {
+            const currArr = currency.filter((curr) => curr.id == id);
+            this.symbol = currArr[0].symbol;
+            console.log(currArr[0].symbol);
+        },
+        async FetchCurrensiesData({ page, itemsPerPage }) {
+            this.loading = true;
+            setContentType("application/json");
+
+            const response = await axios.get(
+                `/currencies?page=${page}&perPage=${itemsPerPage}&search=${this.Search}`
+            );
+            this.currencies = response.data.data;
+            this.totalItems = response.data.meta.total;
+            this.loading = false;
+        },
+        async CreateCurrency(formData) {
+            console.log(formData);
+            // Adding a custom header to the Axios request
+            setContentType("application/json");
+
+            const config = {
+                method: "POST",
+                url: "/currencies",
+                data: formData,
+            };
+
+            // Using Axios to make a GET request with async/await and custom headers
+            const response = await axios(config);
+            // toast.success("Customer Succesfully Created", {
+            //     autoClose: 1000,
+            // });
+            this.createDailog = false;
+            this.FetchCurrensiesData({
+                page: this.page,
+                itemsPerPage: this.itemsPerPage,
+            });
+        },
+        async DeleteCurrency(id) {
+            const config = {
+                method: "DELETE",
+                url: "/currencies/" + id,
+            };
+
+            const response = await axios(config);
+            // toast.success("Customer Succesfully Deleted", {
+            //     autoClose: 1000,
+            // });
+            this.FetchCurrensiesData({
+                page: this.page,
+                itemsPerPage: this.itemsPerPage,
+            });
+        },
+        async FetchCurrencyData(id) {
+            setContentType("application/json");
+            const response = await axios.get(`/currencies/${id}`);
+
+            this.currency = response.data.data; // Assign the fetched data directly to this.people
+        },
+        async UpdateCurrency(id, data) {
+            console.log(data);
+
+            // Adding a custom header to the Axios request
+            setContentType("application/json");
+
+            const config = {
+                method: "PUT",
+                url: "/currencies/" + id,
+                data: data,
+            };
+
+            // Using Axios to make a post request with async/await and custom headers
+            const response = await axios(config);
+            // toast.success("Customer Succesfully Updated", {
+            //     autoClose: 1000,
+            // });
+
+            this.updateDailog = false;
+            this.FetchCurrensiesData({
+                page: this.page,
+                itemsPerPage: this.itemsPerPage,
+            });
+        },
+    },
+});
