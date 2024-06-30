58bded6dd7ebc37d5f4b2c93c0e5e10ee07b3a74
farzadhakimi1402 <farzadhakimi1402@gmail.com>
2024-06-30 14:27:02 +0430

    expense people account

--- app/Http/Resources/Finances/ExpensePaymentSentResource.php
+++ app/Http/Resources/Finances/ExpensePaymentSentResource.php
@@ -14,6 +14,7 @@ class ExpensePaymentSentResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        
         return [
             'id' => $this->id,
             'reference' => $this->reference,
